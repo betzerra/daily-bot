1675de1742b6f63105288846ec691d3c4f51fdd4
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2021-02-24 23:16:03 -0300

    Changed version on start.sh (this is for my Rpi)

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd ${__dir}
-source /home/pi/.rvm/environments/ruby-2.6.2;
+source /home/pi/.rvm/environments/ruby-2.7.2;
 bundle exec ruby app.rb
