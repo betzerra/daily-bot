10989f6fee77dccfebf9514018ddef516affdff7
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2021-02-25 22:00:14 -0300

    Added daily increments

--- lib/vaccines_entry.rb
+++ lib/vaccines_entry.rb
@@ -1,10 +1,29 @@
+require 'date'
+require "time"
+
 class VaccinesEntry
-    attr_accessor :sum_first_doses, :sum_second_doses
+    attr_accessor :sum_first_doses, :sum_second_doses, :sum_today_first_doses, :sum_today_second_doses, :last_day
 
     def initialize(payload)
         entries = payload.flat_map { |i| i[1]["fechas"] }
+        @last_day = Time.parse(entries.last['fecha']).to_date
 
         @sum_first_doses = entries.reduce(0) { |sum, i| sum + i["primeradosis"] }
         @sum_second_doses = entries.reduce(0) { |sum, i| sum + i["segundadosis"] }
+
+        @sum_today_first_doses = entries
+                                    .filter { |i| VaccinesEntry.is_entry_date_same_day(i['fecha'], @last_day) }
+                                    .reduce(0) { |sum, i| sum + i["primeradosis"] }
+
+        @sum_today_second_doses = entries
+                                    .filter { |i| VaccinesEntry.is_entry_date_same_day(i['fecha'], @last_day) }
+                                    .reduce(0) { |sum, i| sum + i["segundadosis"] }
+    end
+
+    private
+
+    def self.is_entry_date_same_day(origin, date)
+        lhs = Time.parse(origin)
+        lhs.to_date === date
     end
 end
--- lib/vaccines_step.rb
+++ lib/vaccines_step.rb
@@ -5,9 +5,12 @@ class VaccinesStep < TelegramStep
   
     def handle_step
         text = "*Vacunaciones contra COVID-19 en Argentina* 💉 \n"\
-            "*- Primera dosis*: #{thousand_format(vaccines.sum_first_doses)} \n"\
-            "*- Segunda dosis*: #{thousand_format(vaccines.sum_second_doses)} \n\n"\
-            "[Fuente](https://covidstats.com.ar/vacunados)"
+            "*- Primera dosis*: #{thousand_format(vaccines.sum_first_doses)} "\
+            "#{today_doses(vaccines.sum_today_first_doses)}\n"\
+            "*- Segunda dosis*: #{thousand_format(vaccines.sum_second_doses)} "\
+            "#{today_doses(vaccines.sum_today_second_doses)}\n\n"\
+            "[Fuente](https://covidstats.com.ar/vacunados) \n"\
+            "Última actualización: #{last_day}"
 
         send_message(text)
     end
@@ -19,5 +22,15 @@ class VaccinesStep < TelegramStep
 
     def vaccines
         @vaccines ||= VaccinesEntry.new(request_vaccines)
+    end
+
+    def last_day
+        vaccines.last_day.strftime('%d-%m-%Y')
+    end
+
+    def today_doses(number)
+        return if number.zero?
+
+        "(+#{thousand_format(number)})"
     end
 end
